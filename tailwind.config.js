47e0495e79389066a6d7680451fb66d48e85c1b6
1shChheda <1shvenom786@gmail.com>
2023-03-06 09:29:37 +0530

    03__tailwind.config__tailwindcss-intellisense

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        mammoth : ['8rem', { lineHeight: '5rem' }]
+      }
+    },
   },
   plugins: [],
 }
